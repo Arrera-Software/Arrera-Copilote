d356315f3a31e2e721a9cae0aab26f09acf8bd3d
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-04-28 14:04:51 +0200

    Ajout icon a l'interface

--- copilote/arreracopilote.py
+++ copilote/arreracopilote.py
@@ -14,6 +14,12 @@ class ArreraCopilote :
         self.__screen.title("Arrera : Copilote")
         self.__screen.maxsize(700,700)
         self.__screen.minsize(700,700)
+        # emplacement icon 
+        self.__iconSix = "asset/icon/six/logo-normal.png"
+        self.__iconRyley = "asset/icon/ryley/icon.png"
+        self.__iconCopilote = "asset/icon/copilote/icon.png"
+        # Ajout icon a la fenetre
+        self.__screen.iconphoto(False,PhotoImage(file=self.__iconCopilote))
 
     def active(self):
         self.__screen.mainloop()
