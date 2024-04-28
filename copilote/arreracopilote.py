f4332063b0e3f61c0ac6eaba87cfd22fe01f7d02
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-04-28 18:22:30 +0200

    Gui frameTop  frameReponse frameInput

--- copilote/arreracopilote.py
+++ copilote/arreracopilote.py
@@ -1,4 +1,5 @@
 from ObjetsNetwork.arreraNeuron import*
+from PIL import Image, ImageTk
 
 class ArreraCopilote :
     def __init__(self):
@@ -9,27 +10,58 @@ class ArreraCopilote :
         self.__assistantRyley = ArreraNetwork("configuration/configUser.json",
                                           "configuration/ryley.json",
                                           "configuration/listFete.json")
+        # varriable couleur
+        self.__mainColor = "white"
+        self.__textMainColor = "black"
         #fenetre tkinter
         self.__screen = Tk()
         self.__screen.title("Arrera : Copilote")
         self.__screen.maxsize(700,700)
         self.__screen.minsize(700,700)
         # emplacement icon 
-        self.__iconSix = "asset/icon/six/logo-normal.png"
-        self.__iconRyley = "asset/icon/ryley/icon.png"
-        self.__iconCopilote = "asset/icon/copilote/icon.png"
+        self.__emplacementIconSix = "asset/icon/six/logo-normal.png"
+        self.____emplacementIconRyley = "asset/icon/ryley/icon.png"
+        self.__emplacementIconCopilote = "asset/icon/copilote/icon.png"
         # Ajout icon a la fenetre
-        self.__screen.iconphoto(False,PhotoImage(file=self.__iconCopilote))
+        self.__screen.iconphoto(False,PhotoImage(file=self.__emplacementIconCopilote))
         # Mise en place de l'interface
         frameTop = Frame(self.__screen,bg="red",width=700,height=100)
         frameReponse =  Frame(self.__screen,bg="blue",width=700,height=450)
         frameInput = Frame(self.__screen,bg="orange",width=700,height=100)
         frameBottom = Frame(self.__screen,bg="green",width=700,height=50)
-        # Affichage
+        # Widget frameTop
+        labelTitle = Label(frameTop,text="Arrera Copilote",bg=self.__mainColor,fg=self.__textMainColor,font=("arial","35"))
+        # Widget frameReponse
+        labelIconRyley = Label(frameReponse)
+        labelIconSix = Label(frameReponse)
+        iconSix = ImageTk.PhotoImage((Image.open(self.__emplacementIconSix).resize((60,60))))
+        iconRyley = ImageTk.PhotoImage((Image.open(self.____emplacementIconRyley).resize((60,60))))
+        labelIconSix.image_names = iconSix
+        labelIconRyley.image_names = iconRyley
+        labelIconSix.configure(image=iconSix)
+        labelIconRyley.configure(image=iconRyley)
+        labelReponseSix = Label(frameReponse,font=("arial","15"),bg="grey",text="Reponse SIX")
+        labelReponseRyley = Label(frameReponse,font=("arial","15"),bg="grey",text="Reponse RYLEY")
+        # Widget frameInput
+        input = Frame(frameInput)
+        entryInput = Entry(input,width=35,font=("arial","20"),relief=SOLID)
+        btnSend = Button(input,text="Envoyer",bg=self.__mainColor,fg=self.__textMainColor,font=("arial","15"))
+        # Affichage general
+        # Frame
         frameTop.pack(side="top")
         frameReponse.pack()
         frameInput.pack()
         frameBottom.pack(side="bottom")
+        # Widget
+        labelTitle.place(relx=0.5,rely=0.5,anchor="center")
+        labelIconSix.place(x=25,y=25)
+        labelIconRyley.place(x=25,y=225)
+        labelReponseSix.place(x=90,y=25)
+        labelReponseRyley.place(x=90,y=225)
+        entryInput.pack(side="left")
+        btnSend.pack(side="right")
+        input.place(relx=0.5,rely=0.5,anchor="center")
+
 
     def active(self):
         self.__screen.mainloop()
