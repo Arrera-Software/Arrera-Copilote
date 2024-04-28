e43625bb0230c50c1f0639b529c780e1892519ed
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-04-28 14:01:04 +0200

    Ajout commentaire

--- copilote/arreracopilote.py
+++ copilote/arreracopilote.py
@@ -2,13 +2,14 @@ from ObjetsNetwork.arreraNeuron import*
 
 class ArreraCopilote :
     def __init__(self):
+        #objet assistant
         self.__assistantSix = ArreraNetwork("configuration/configUser.json",
                                           "configuration/six.json",
                                           "configuration/listFete.json")
         self.__assistantRyley = ArreraNetwork("configuration/configUser.json",
                                           "configuration/ryley.json",
                                           "configuration/listFete.json")
-        
+        #fenetre tkinter
         self.__screen = Tk()
         self.__screen.title("Arrera : Copilote")
         self.__screen.maxsize(700,700)
