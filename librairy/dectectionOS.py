76fdc2469a0b7c8172990151369fe54379af7cce
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-04-29 18:34:12 +0200

    Ajout parametre

--- librairy/dectectionOS.py
+++ librairy/dectectionOS.py
@@ -2,16 +2,16 @@ import platform
 
 class OS :
     def __init__(self) :
-        self.__os = platform.system()
+        self.os = platform.system()
         
     def osWindows(self):
-        if self.__os == "Windows":
+        if self.os == "Windows":
            return True
         else :
             return False
     
     def osLinux(self):
-        if self.__os == "Linux":
+        if self.os == "Linux":
             return True
         else :
             return False
