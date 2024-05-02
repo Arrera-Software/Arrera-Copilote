ae87878b3afcceb4c1f98dda11a676753488a14d
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-05-02 17:18:31 +0200

    Ajout setCase

--- copilotefonction/copiloteTableur.py
+++ copilotefonction/copiloteTableur.py
@@ -1,6 +1,7 @@
 from fonctionnalites.arreratableur import*
 from tkinter import*
 from tkinter import filedialog, messagebox
+import re
 
 
 class CArreraCopiloteTableur :
@@ -41,17 +42,21 @@ class CArreraCopiloteTableur :
                                       fg=self.__textColor,bg=self.__color,
                                       )
         #Cadre secondaire
-        frameCase = Frame(self.__mainFrame,bg=self.__color)
+        self.__frameCase = Frame(self.__mainFrame,bg=self.__color)
         frameAction =  Frame(self.__mainFrame,bg=self.__color)
         # widget mainframe
         labelIndication = Label(self.__mainFrame,text="Modifier le tableur",
                                 font=("arial",15),fg=self.__textColor,bg=self.__color)
+        self.__labelCaseSelect = Label(self.__mainFrame,font=("arial",15),
+                                       fg=self.__textColor,bg=self.__color)
+        self.__labelfncSelect = Label(self.__mainFrame,font=("arial",15),
+                                       fg=self.__textColor,bg=self.__color)
         # widget frameCase
-        labelCase = Label(frameCase,text="Case :",font=("arial",15),
+        labelCase = Label(self.__frameCase,text="Case :",font=("arial",15),
                           fg=self.__textColor,bg=self.__color)
-        self.__entryCase = Entry(frameCase,font=("arial","15"),width=5,relief=SOLID)
-        btnSetCase = Button(frameCase,text="Valider",
-                            bg=self.__color,fg=self.__textColor,font=("arial","15"))
+        self.__entryCase = Entry(self.__frameCase,font=("arial","15"),width=5,relief=SOLID)
+        btnSetCase = Button(self.__frameCase,text="Valider",bg=self.__color,
+                            fg=self.__textColor,font=("arial","15"),command=self.__setCase)
         # widget mainFrame 
         labelAction = Label(frameAction,text="Action :",font=("arial",15),
                           fg=self.__textColor,bg=self.__color)
@@ -60,11 +65,10 @@ class CArreraCopiloteTableur :
                             bg=self.__color,fg=self.__textColor,font=("arial","15"))
         #set option menu 
         self.__type.set(listAction[0])
-        
+        # Affichage des valeur du tableur
         self.__updateTableur()
-        #Affichage 
-        
-        
+        # Affichage 
+        # Affichage widget mainFrame
         labelIndication.place(x=0,y=0)
         labelCase.pack(side="left")
         self.__entryCase.pack(side="left")
@@ -72,16 +76,46 @@ class CArreraCopiloteTableur :
         labelAction.pack(side="left")
         choixAction.pack(side="left")
         btnSetAction.pack(side="right")
-        frameCase.place(x=15,y=35)
+        self.__frameCase.place(x=15,y=35)
         frameAction.place(x=15,y=85)
+        # Affichage principale
         self.__mainFrame.pack(side="left")
         self.__labelTableurView.pack(side="left",anchor="n")
     
+    def __verifChaine(self,chaine):
+        # Expression régulière pour vérifier la chaîne
+        regex = r"^[A-Z]\d$"
+
+        # Vérification de la chaîne avec l'expression régulière
+        if re.match(regex, chaine):
+            return True
+        else:
+            return False
+
     def __updateTableur(self):
         sortie = self.__tableur.read()
         self.__labelTableurView.configure(text="Contenu du fichier tableur", justify="left")
         for cell_position, cell_value in sortie.items():
             text = self.__labelTableurView.cget("text")
-            self.__labelTableurView.configure(text=text+"\n"+f"Cellule {cell_position} : {cell_value}", justify="left")
+            self.__labelTableurView.configure(text=text
+                                              +"\n"+f"Cellule {cell_position} : {cell_value}", 
+                                              justify="left")
+    
+    def __setCase(self):
+        case = self.__entryCase.get()
+        if not case  :
+            messagebox.showerror("Case est vide","Veillez entrez quelque chose dans la case")
+        else :
+            if (self.__verifChaine(case)==True):
+                self.__caseSelect = case 
+                self.__labelCaseSelect.configure(text="Case selectionner : "+case)
+                self.__frameCase.place_forget()
+                self.__labelCaseSelect.place(x=15,y=35)
+
+            else :
+                messagebox.showerror("Case invalide","La case n'est pas valide")
+        self.__entryCase.delete(0,END)
+    
+    
 
         
