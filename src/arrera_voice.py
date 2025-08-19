ecfd48d5a339ad14d97684d4d117e4106e84a832
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-08-19 21:07:46 +0200

    Mise a niveau d'Arrera Voice

--- src/arrera_voice.py
+++ src/arrera_voice.py
@@ -3,19 +3,23 @@ from pyttslib import TextToSpeech
 from ObjetsNetwork.network import *
 from librairy.travailJSON import jsonWork
 import speech_recognition as sr
+import os
+import sys
+import platform
+import subprocess
 
 
 class CArreraVoice:
-    def __init__(self,configFile:jsonWork):
-        self.__configFile = configFile
-        self.__emplacementSoundMicro = self.__configFile.lectureJSON("fileMicro")
+    def __init__(self,config:str,microFile:str):
+        self.__configFile = jsonWork(self.__resource_path(config))
+        self.__emplacementSoundMicro = self.__resource_path(microFile)
         self.__soundMicro = True
         self.__listWord = []
         self.__nbWord = 0
         self.__outPutText = ""
         self.loadConfig()
 
-        if network().getEtatInternet() == True:
+        if network().getEtatInternet():
             self.__tts = TextToSpeech(engine="google")
             self.__tts.set_voice("fr")
         else:
@@ -25,7 +29,13 @@ class CArreraVoice:
             })
             self.__tts.set_voice("French (France)")
 
-
+    def __resource_path(self, relative_path):
+        if platform.system() == "Darwin":
+            if hasattr(sys, '_MEIPASS'):
+                return os.path.join(sys._MEIPASS, relative_path)
+            return os.path.join(os.path.abspath("."), relative_path)
+        else:
+            return relative_path
 
     def loadConfig(self):
         if (self.__configFile.lectureJSON("soundMicro") == "1" ):
@@ -39,11 +49,24 @@ class CArreraVoice:
         self.__tts.speak(text)
 
     def playFile(self,file:str):
-        pl(file)
+        if platform.system() == "Darwin":
+            try :
+                subprocess.run(["afplay", self.__resource_path(file)], check=True)
+                return True
+            except subprocess.CalledProcessError as e:
+                return False
+                #print(f"Error playing sound file: {e}")
+        else :
+            try :
+                pl(file)
+                return True
+            except Exception as e:
+                #print(f"Error playing sound file: {e}")
+                return False
 
     def listen(self):
         if self.__soundMicro:
-            pl(self.__emplacementSoundMicro)
+            self.playFile(self.__emplacementSoundMicro)
 
         r = sr.Recognizer()
         with sr.Microphone() as source:
