c4fae51845f9d464a8d12a38a52980a86dfacbdc
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-05-19 10:01:39 +0200

    Ajout du numero de version

--- src/copilote_assistant.py
+++ src/copilote_assistant.py
@@ -6,7 +6,7 @@ from librairy.arrera_tk import *
 
 THEME_FILE = "asset/theme/theme_bleu_violet.json"
 
-VERSION = "dev"
+VERSION = "I2026-2.00"
 
 class copilote_assistant():
     def __init__(self):
